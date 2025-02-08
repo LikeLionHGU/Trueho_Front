35bf8db1550cdcbe61f141418b99d4f17133d2d9
Haeun <hahashe@handong.ac.kr>
2025-02-08 13:46:28 +0900

    feat : 푸터 하단 고정 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Loginpage from './routes/login';
+import Header from "./components/header";
+import Footer from "./components/footer";
 
 function App() {
 
   return (
-    <div>
+    <div id="root">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Loginpage />} />
-        </Routes>
+        {/* 모든 페이지에 헤더가 보일수있게 고정*/}
+        <Header />
+
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<Loginpage />} />
+          </Routes>
+        </div>
+        {/* 모든 페이지에 푸터가 보일수있게 고정*/}
+        <Footer />
       </BrowserRouter>
     </div>
+
   );
 }
 
