04ee66dd6840be8ecac191df6f8af75e7db3d954
Haeun <hahashe@handong.ac.kr>
2025-02-08 13:17:54 +0900

    feat : header js, css 파일 생성

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import styles from "./styles/header.css";
+
+function Header() {
+  
+
+  return (
+    <div className="header-container">
+      
+    </div>
+  );
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+
 import Footer from './components/footer';
+import Header from './components/header';
+
 import styles from './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Header />
     <App />
     <Footer />
   </React.StrictMode>
