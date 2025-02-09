ee1719ec52b7fc6be3d2cd4ffbc5809440b86c6f
Haeun <hahashe@handong.ac.kr>
2025-02-09 19:41:32 +0900

    style : 졸업생 로그인 페이지 ui 작업

--- src/pages/gradLogin.js
+++ src/pages/gradLogin.js
@@ -1,52 +1,47 @@
-import styles from "../styles/login.css";
-import loginHansumLogo from "../assets/Page/Login/hansum_logo.png"
-import loginHansumLogoImg from "../assets/Page/Login/hansum_logo_image.png"
-import loginHansumLogoText from "../assets/Page/Login/hansum_logo_text.png"
-import loginGoogle from "../assets/Page/Login/google.png"
+import styles from "../styles/gradLogin.css";
+import gradHansumLogo from "../assets/Page/GradLogin/hamsum_logo.png"
+
+// import Header from "../components/header";
 
 function Loginpage() {
   
 
   return (
-    <div className="login-container">
-      <div className="login-lar-container1">
+    <>
+    {/* <Header /> */}
+    <div className="gradLogin-container">
+      <div className="gradLogin-lar-container1">
         {/* 왼쪽 */}
-        <div className="login-med-container1">
-          <div className="login-med-container1-text1">
-            <img src={loginHansumLogo} />
-            <span>이란?</span>
+        <div className="gradLogin-med-container1">
+          <div className="gradLogin-med-container1-text1">
+            <span>졸업생 인증</span>
           </div>
-          <div className="login-med-container1-text2">
-            <p>한섬은 졸업생과 재학생을 연결하여 멘토링을 돕는 플랫폼으로, </p>
-            <p>선배들이 후배들에게 자신의 경험을 나누고 조언을 제공할 수 있도록 돕습니다.</p>
-          </div>
-          <div className="login-med-container1-text3-btn">
-            <span>더 자세한 설명보기</span>
+          <div className="gradLogin-med-container1-text2">
+            <p>졸업생 인증을 원하시면 여기로 연락주세요</p>
+            <p><span>0000000@handong.ac.kr</span></p>
           </div>
         </div>
         {/* 오른쪽 */}
-        {/* <div className="login-med-container2">
-          <div className="login-med-container2-icon1">
-            <img src={loginHansumLogoText} />
+        <div className="gradLogin-med-container2">
+          <div className="gradLogin-med-container2-icon1">
+            <span>Welcome to</span>
           </div>
-          <div className="login-med-container2-icon2">
-            <img src={loginHansumLogoImg} />
+          <div className="gradLogin-med-container2-icon2">
+            <img src={gradHansumLogo} />
           </div>
-        </div> */}
+        </div>
       </div>
 
 
-      <div className="login-lar-container2">
-        <div className="login-small-container1">
-          <img src={loginGoogle} />
-          <span>한동대 메일로 로그인</span>
-        </div>
-        <div className="login-small-container2">
-          <p>졸업생이신가요? <span>졸업생 로그인</span> 하러 가기</p>
+      <div className="gradLogin-lar-container2">
+        <div className="gradLogin-small-container1">
+          <span>뒤로가기</span>
         </div>
       </div>
     </div>
+    </>
+    
   );
-}
+} 
 
 export default Loginpage;
