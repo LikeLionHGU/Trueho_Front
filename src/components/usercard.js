698aa1fa2719e5f1e7eb2bdec3b1e0a0015758a3
jaeho777 <leedokgun@gmail.com>
2025-02-12 02:32:12 +0900

    add: 상세페이지 css

--- src/components/usercard.js
+++ src/components/usercard.js
@@ -1,15 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "../components/styles/usercard.css";
 
-function usercard({ name, major, jobTitle, profileImage }) {
+function UserCard({ name, major, jobTitle, profileImage }) {
+  const navigate = useNavigate();
+
   return (
-    <div className="user-card">
+    <div className="user-card" onClick={() => navigate(`/user/${name}`)}>
       <img src={profileImage} alt={`${name} profile`} className="user-profile" />
       <h3>{name}</h3>
-      <p>{major}</p>
+      <p className="user-major">{major}</p>
       <h4>{jobTitle}</h4>
     </div>
   );
 }
 
-export default usercard;
+export default UserCard;
