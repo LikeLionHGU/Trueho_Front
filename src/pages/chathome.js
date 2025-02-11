43972a780ac44d032a700acc7c2b3d931074c4ff
Haeun <hahashe@handong.ac.kr>
2025-02-11 18:30:20 +0900

    style : chathome ui 디자인

--- src/pages/chathome.js
+++ src/pages/chathome.js
@@ -1,7 +1,8 @@
+import ChatList from "../components/chatpagelist";
 import styles from "../styles/chathome.css";
 // import gradHansumLogo from "../assets/Page/GradLogin/hamsum_logo.png"
 
-import Footer from "../components/footer";
+
 
 
 function ChatHomepage() {
@@ -10,10 +11,14 @@ function ChatHomepage() {
   return (
     <>
     <div>
-      <span>기본 파일 입니다 !</span>
+      <div className="chatHome-container">
+        <div className="chatHome-innercontainer">
+          <ChatList />
+          
+        </div>
+      </div>
     </div>
 
-    <Footer />
     </>
   );
 }
