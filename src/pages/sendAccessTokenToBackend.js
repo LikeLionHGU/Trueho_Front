39c136ca9a27541835341ec5bab2892468b33558
Haeun <hahashe@handong.ac.kr>
2025-02-12 21:00:44 +0900

    feat : 로그인 백엔드로 토큰 보내기 성공

--- src/pages/sendAccessTokenToBackend.js
+++ src/pages/sendAccessTokenToBackend.js
@@ -1,7 +1,21 @@
 import axios from "axios";
 
 const sendAccessTokenToBackend = async (idToken) => {
-  console.log(idToken);
+  try {
+    const serverResponse = await axios.post(
+      `${process.env.REACT_APP_HOST_URL}/main/login?id_token=${idToken}`, // id_token을 쿼리 파라미터로 전달
+      {}, // 요청 본문은 빈 객체
+     
+    );
+
+    console.log("Login successful with server response:", serverResponse);
+    localStorage.setItem("memberId", serverResponse.data.memberId);
+
+    return serverResponse.data;
+  } catch (error) {
+    console.error("Login failed with error:", error);
+    throw error;
+  }
 };
 
 export default sendAccessTokenToBackend;
