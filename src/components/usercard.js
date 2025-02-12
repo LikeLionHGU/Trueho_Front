3957b1a2a197ffcfda0f9ec5d1b352df3002976b
jaeho777 <leedokgun@gmail.com>
2025-02-12 15:23:39 +0900

    add: build 파일 추가

--- src/components/usercard.js
+++ src/components/usercard.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "../components/styles/usercard.css";
+// import profilelogo from "../components/Profile/profilelogo.png";
 
-function UserCard({ name, major, jobTitle, profileImage }) {
+function UserCard({ name, major, jobTitle, profilelogo }) {
   const navigate = useNavigate();
 
   return (
     <div className="user-card" onClick={() => navigate(`/user/${name}`)}>
-      <img src={profileImage} alt={`${name} profile`} className="user-profile" />
+      <img src={profilelogo} alt={`${name} profile`} className="user-profile" />
       <h3>{name}</h3>
       <p className="user-major">{major}</p>
       <h4>{jobTitle}</h4>
