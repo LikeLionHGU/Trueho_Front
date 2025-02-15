d6bf3336ade1c4c32ca3a8685083ee1686b67271
Haeun <hahashe@handong.ac.kr>
2025-02-16 01:35:37 +0900

    feat : 스타일 수정 및 높이 수정, 버튼 기능 구현, 사진 입력 기능 구현

--- src/pages/Newprofile.js
+++ src/pages/Newprofile.js
@@ -27,7 +27,7 @@ function Newprofile() {
 
 // 2) 입력 받은 값 배열에 넣기
   const [data, setData] = useState({
-    hansum: "0",
+    hansum: "2",
     name: "0",
     admission: "0",
     graduation: "0",
@@ -38,7 +38,7 @@ function Newprofile() {
         name: "0",
         detail: "0",
       }, ],
-    showing: "0",
+    showing: "2",
   });
 
   const onChangeInput = (input) => {
@@ -48,7 +48,40 @@ function Newprofile() {
     });
   };
 
-// 3) 배열 값 post 하는 함수
+// 3) 한섬 한내기 클릭 했을때 값 저장
+
+  const handleClickHansum = () => {
+    setData({
+      ...data,
+      hansum: "1", // 한섬 클릭 시 hansum 값을 1로 변경
+    });
+  };
+
+  const handleClickHannegi = () => {
+    setData({
+      ...data,
+      hansum: "0", // 한내기 클릭 시 hansum 값을 0으로 변경
+    });
+  };
+
+// 4) 프로필 공개 비공개 
+
+const handleClickShow = () => {
+  setData({
+    ...data,
+    showing: "1", // 1이면 공개
+  });
+};
+
+const handleClickNoShow = () => {
+  setData({
+    ...data,
+    showing: "0", // 0이면 비공개
+  });
+};
+
+
+// 5) 배열 값 post 하는 함수
   const postDataToJSONFile = (e) => {
     e.preventDefault();
     console.log("보낼 데이터:", data); // 확인용 로그
@@ -66,7 +99,30 @@ function Newprofile() {
       alert("에러가 발생했습니다.");
     });
   };
-  
+
+// 6) 사진 업로드 + 사진 미리보기
+  const [uploadImgFile, setUploadImgFile] = useState("");
+  const imgRef = useRef();
+
+  const saveImgFile = () => {
+    // 입력된 파일을 가져온다.
+    const file = imgRef.current.files[0];
+
+    console.log(file);//업로드한 파일의 정보가 보여진다!
+
+    //파일 데이터를 읽어올 수 있도록 FileReader 생성자 만들기.
+    const reader = new window.FileReader();
+
+    //readAsDataURL - File, Blob을 읽어와 base64로 인코딩 한 문자열을 FileReader 인스턴스의 result라는 속성에 담아준다.
+    //간단히 File, Blob 객체를 읽어 Data URL 형태로 바꿔준다!
+    reader.readAsDataURL(file);
+
+    //onload - 파일을 성공적으로 읽었을 때 실행.
+    reader.onload = () => {
+        //파일이 읽어졌다면 result 속성에 담기게 된다.
+      setUploadImgFile(reader.result);
+    };
+  };
   
 
   return (
@@ -81,7 +137,7 @@ function Newprofile() {
 
         <div className="newprofile-top-container-second box-row">
 
-          <div className="newprofile-top-container-second-selectbox">
+          <div className={`newprofile-top-container-second-selectbox ${data.hansum === "1" ? "active" : ""}`} onClick={handleClickHansum}>
             <img src={profileHansum} />
             <div className="newprofile-top-container-second-selectbox-text" >
               <span>한섬</span>
@@ -90,14 +146,15 @@ function Newprofile() {
           </div>
 
           <div className="newprofile-top-container-second-selectbox-margin"></div>
-          <div className="newprofile-top-container-second-selectbox">
+
+          <div className={`newprofile-top-container-second-selectbox ${data.hansum === "0" ? "active" : ""}`} onClick={handleClickHannegi}>
             <img src={profileHannegi} />
             <div className="newprofile-top-container-second-selectbox-text" >
               <span>한내기</span>
               <p>한동 새내기로써 멘티로 등록을 원해요</p>
             </div>
           </div>
-          
+
         </div>
       </div>
 
@@ -109,12 +166,35 @@ function Newprofile() {
           </div>
 {/* 2------------------------------------------------------------ */}
           <div className="newprofile-bottom-container-2 box-row">
+
+
+            {/* <div className="newprofile-bottom-container-2-pic box-column"> */}
+              {/* <img src={profiledefault} className="newprofile-bottom-container-2-pic-img"/> */}
+              {/* <img src={uploadImgFile || profiledefault} alt="이미지 미리보기" className="newprofile-bottom-container-2-pic-img"/>
+              <input className="newprofile-bottom-container-2-pic-btn" type="file" onChange={saveImgFile} ref={imgRef} accept="image/*" >
+                사진 등록
+              </input>
+            </div> */}
+
+
             <div className="newprofile-bottom-container-2-pic box-column">
-              <img src={profiledefault} className="newprofile-bottom-container-2-pic-img"/>
-              <div className="newprofile-bottom-container-2-pic-btn">
+              <img src={uploadImgFile || profiledefault} alt="이미지 미리보기" className="newprofile-bottom-container-2-pic-img" />
+              
+              {/* input과 span을 분리 */}
+              <label htmlFor="file-upload" className="newprofile-bottom-container-2-pic-btn">
                 <span>사진 등록</span>
-              </div>
+              </label>
+              <input 
+                id="file-upload" 
+                className="newprofile-bottom-container-2-pic-btn-input" 
+                type="file" 
+                onChange={saveImgFile} 
+                ref={imgRef} 
+                accept="image/*" 
+                style={{ display: 'none' }} 
+              />
             </div>
+
             <div className="newprofile-bottom-container-2-njjall box-column">
               {/* 닉네임 입력 */}
               <div className="newprofile-bottom-container-2-njj box-row">
@@ -125,7 +205,7 @@ function Newprofile() {
                 {/* input 위에 별표와 인풋박스 */}
                 <div className='container_2'>
                   <div className="newprofile-bottom-container-2-njj-input box-column">
-                    <span>*</span>
+                    <div className='star'><span>*</span></div>
                     <input placeholder="텍스트를 입력하세요" type="text" name="name" onChange={onChangeInput}/>
                   </div>
                   </div>
@@ -138,7 +218,7 @@ function Newprofile() {
                 {/* input 위에 별표와 인풋박스 */}
                 <div className='container_2 box-row'>
                   <div className="newprofile-bottom-container-2-njj-input box-column">
-                    <span>*</span>
+                  <div className='star'><span>*</span></div>
 
                     <select name="admission" className="newprofile-bottom-container-2-njj-input-select" value={data.admission} onChange={onChangeInput}>
                       <option value="" >입학 연도</option>
@@ -151,7 +231,7 @@ function Newprofile() {
                 <div className="padding-top"><span> ~ </span></div>
                 
                 <div className="newprofile-bottom-container-2-njj-input box-column">
-                  <span>*</span>
+                <div className='star'><span>*</span></div>
 
                   <select name="graduation" className="newprofile-bottom-container-2-njj-input-select" onChange={onChangeInput} value={data.graduation}>
                     <option value="">졸업 연도</option>
@@ -172,7 +252,7 @@ function Newprofile() {
                 {/* input 위에 별표와 인풋박스 */}
                 <div className='container_2'>
                   <div className="newprofile-bottom-container-2-njj-input box-column">
-                    <span>*</span>
+                  <div className='star'><span>*</span></div>
                     <select name="major" className="newprofile-bottom-container-2-njj-input-selectmajor" onChange={onChangeInput} value={data.major}>
                       <option value="">학부 선택</option>
                       <option value="기계제어공학부">기계제어공학부</option>
@@ -200,7 +280,7 @@ function Newprofile() {
             </div>
                 {/* input 위에 별표와 인풋박스 */}
             <div className="newprofile-bottom-container-3-input box-column">
-              <span>*</span>
+            <div className='star'><span>*</span></div>
               <input placeholder="현재 직무를 작성해 주세요 (ex. 마케터, 웹디자이너)" type="text" name="work" onChange={onChangeInput}></input>
             </div>
           </div>
@@ -238,17 +318,17 @@ function Newprofile() {
           </div>
 
           <div className="newprofile-bottom-container-6-second box-row">
-            <div className="newprofile-bottom-container-6-second-selectbox">
+            <div className={`newprofile-bottom-container-6-second-selectbox ${data.showing === "1" ? "active" : ""}`} onClick={handleClickShow}>
               <img src={profileactive} />
-              <div className="newprofile-bottom-container-6-second-selectbox-text">
+              <div className={`newprofile-bottom-container-6-second-selectbox-text ${data.showing === "1" ? "active" : ""}`}>
                 <span>프로필 공개</span>
                 <p>프로필을 공개하면 한섬들 목록에 올라가며, 메시지를 받습니다</p>
               </div>
             </div>
             <div className="newprofile-bottom-container-6-second-selectbox-margin"></div>
-            <div className="newprofile-bottom-container-6-second-selectbox">
+            <div className={`newprofile-bottom-container-6-second-selectbox ${data.showing === "0" ? "active" : ""}`} onClick={handleClickNoShow}>
               <img src={profiledeactive} />
-              <div className="newprofile-bottom-container-6-second-selectbox-text">
+              <div className={`newprofile-bottom-container-6-second-selectbox-text ${data.showing === "0" ? "active" : ""}`}>
                 <span>프로필 비공개</span>
                 <p>프로필을 공개하면 한섬들 목록에 올라가지 않습니다</p>
               </div>
