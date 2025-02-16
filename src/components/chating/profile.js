ef70bb37ea1c2ca538f661eaa3d4ccc164ecd2bd
Haeun <hahashe@handong.ac.kr>
2025-02-17 00:17:42 +0900

    feat : 채팅창 profile 사진과 닉네임 가져오기 성공

--- src/components/chating/profile.js
+++ src/components/chating/profile.js
@@ -1,19 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
 import "./profile.css";
 import profileImg from "../../assets/Components/Chating/profile.svg";
 
 
 
-function profile() {
+function Profile() {
+
+  const { id } = useParams(); 
+// 1) 프로필 이미지와 닉넴 가져오는 함수
+  const [data, setData] = useState({ name: "", imgUrl: profileImg });
+  const getData = async () => {
+    try {
+      const res = await axios.get(`${process.env.REACT_APP_HOST_URL}/chat/profile/${id}`);
+
+      const profiledata = res.data;
+      setData(profiledata);
+    } catch (err) {
+      console.error(err);
+    }
+  }; 
+
+    useEffect(() => {
+      getData();
+    }, [id]);
+
   return(
     <>
     <div className="profile-container">
       <div >
-        <img src={profileImg} alt="프로필" className="profile-container-img"/>
+        <img src={data.imgUrl || profileImg} alt="프로필" className="profile-container-img"/>
       </div>
-      <span>춤추는 고라니</span>
+      <span>{data.name || "알 수 없음"}</span>
     </div>
     </>
   );
 }
 
-export default profile;
+export default Profile;
