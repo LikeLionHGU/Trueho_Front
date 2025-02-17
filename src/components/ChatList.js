7366144f8910d3903b48d9218369dc276b3f1fe7
Haeun <hahashe@handong.ac.kr>
2025-02-17 17:03:59 +0900

    feat : chatlist 선택시 색상 변경 기능 구현

--- src/components/ChatList.js
+++ src/components/ChatList.js
@@ -5,68 +5,13 @@ import { useNavigate } from "react-router";
 
 import "./styles/chatlist.css";
 import heart from "../assets/Components/ChatList/heart.svg"
+import whiteheart from "../assets/Components/ChatList/wheart.svg"
+
+
 import defaultprofile from "../assets/Components/ChatList/listprofile.svg"
 
 
 function ChatList() {
-
-  const test_chatlist = [
-    {
-      id  : 1 , 
-      name : "잘생긴라이언" ,  
-      imgUrl : "img_url1" ,
-      lastChat : "마지막 메시지(최대 몇글자 뒤...)" ,
-      flag: 0
-     }, {
-      id  : 2 , 
-      name : "잘생긴사자" ,  
-      imgUrl : "img_url2" ,
-      lastChat : "마지막 메시지(최대 몇글자 뒤...)" ,
-      flag: 0
-     }, {
-      id  : 3 , 
-      name : "잘생긴호랑이" ,  
-      imgUrl : "img_url3" ,
-      lastChat : "마지막 메시지(최대 몇글자 뒤...)" ,
-      flag: 0
-     }, {
-      id  : 4 , 
-      name : "잘생긴라이언" ,  
-      imgUrl : "img_url1" ,
-      lastChat : "마지막 메시지(최대 몇글자 뒤...)" ,
-      flag: 0
-     }, {
-      id  : 5 , 
-      name : "잘생긴사자" ,  
-      imgUrl : "img_url2" ,
-      lastChat : "마지막 메시지(최대 몇글자 뒤...)" ,
-      flag: 0
-     }, {
-      id  : 6 , 
-      name : "잘생긴호랑이" ,  
-      imgUrl : "img_url3" ,
-      lastChat : "마지막 메시지(최대 몇글자 뒤...)" ,
-      flag: 0
-     }, {
-      id  : 7 , 
-      name : "잘생긴라이언" ,  
-      imgUrl : "img_url1" ,
-      lastChat : "마지막 메시지(최대 몇글자 뒤...)" ,
-      flag: 0
-     }, {
-      id  : 8 , 
-      name : "잘생긴사자" ,  
-      imgUrl : "img_url2" ,
-      lastChat : "마지막 메시지(최대 몇글자 뒤...)" ,
-      flag: 0
-     }, {
-      id  : 9 , 
-      name : "잘생긴호랑이" ,  
-      imgUrl : "img_url3" ,
-      lastChat : "마지막 메시지(최대 몇글자 뒤...)" ,
-      flag: 0
-     },
-  ];
 
 
 
@@ -75,6 +20,7 @@ function ChatList() {
 
   const enterChatRoom = (id) => {
     navigate(`/chatroom/${id}`);
+    console.log("경로 id 확인합니다 "+id);
   };
 
 // 2) url에 있는 id 값 가져오기
@@ -85,10 +31,10 @@ const [list, setList] = useState([]);
 const getList = async () => {
   try {
     const res = await axios.get(`${process.env.REACT_APP_HOST_URL}/chat/list`);
-    // console.log("함수 실행 확인");
-    // console.log(res);
+    console.log("list 함수 실행 확인");
+    console.log(res);
     const chatinglist = res.data;
-    // console.log(chatinglist);
+    console.log(chatinglist);
     setList(chatinglist);
   } catch (err) {
     console.error(err);
@@ -105,22 +51,22 @@ useEffect(() => {
     
     <div className="chatlist-container">
       {list.map((chat) => (
-        <div key={chat.id} className="chatlist-item" onClick={() => enterChatRoom(chat.id)}>
+        <div key={chat.id} className={`chatlist-item ${chat.id === Number(id) ? "active" : ""}`} onClick={() => enterChatRoom(chat.id)}>
 
           <img src={defaultprofile} alt={chat.name} className="chatlist-item-img" />
 
           <div className="chatlist-item-textcontainer">
             <div className="chatlist-item-textcontainer-up">
-              <p className="chatlist-item-textcontainer-up-name">{chat.name}</p>
+              <p className={`chatlist-item-textcontainer-up-name ${chat.id === Number(id) ? "active" : ""}`}>{chat.name}</p>
             </div>
-            <p className="chatlist-item-textcontainer-last">{chat.lastChat}</p>
-            
+            <p className={`chatlist-item-textcontainer-last ${chat.id === Number(id) ? "active" : ""}`}>{chat.lastChat}</p>
+  
           </div>
 
           <div className="chatlist-item-check">
-            <img src={heart} alt="heart" className="chatlist-item-textcontainer-up-img" />
+            <img src={chat.id === Number(id) ? whiteheart : heart} alt="heart" className="chatlist-item-textcontainer-up-img" />
           </div>
-
+          
         </div>
       ))}
     </div>
