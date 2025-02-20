3a8d27219b6c9665cdaf59b5bbc20595f7249143
Haeun <hahashe@handong.ac.kr>
2025-02-20 23:23:13 +0900

    feat : 사진 파일명 수정

--- src/pages/Beforemessage.js
+++ src/pages/Beforemessage.js
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 import "../styles/Beforemessage.css";
 import chatBot from "../assets/Components/ChatBot/Chatbot.svg"
-import Logo from "../assets/Components/Before/beforehansum.svg"
+import Logo from "../assets/Components/Before/hansum.svg"
 
 import loginGoogle from "../assets/Page/Login/google.svg"
 
