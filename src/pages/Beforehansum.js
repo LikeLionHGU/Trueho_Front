3578a73a7ba20d0e6e65285fa679b3e4c012df4a
Haeun <hahashe@handong.ac.kr>
2025-02-21 02:46:44 +0900

    fix : 사진 경로 수정

--- src/pages/Beforehansum.js
+++ src/pages/Beforehansum.js
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import "../styles/hansum.css";
 import MajorFilter from "../components/majorfilter";
 import BannerCarousel from "../components/Banner"; // BannerCarousel 컴포넌트 import
-import beforehansum from "../assets/Components/Before/beforehansum.svg";
+import beforehansum from "../assets/Components/Before/background.svg";
 import "../styles/beforehansum.css";
 import loginGoogle from "../assets/Page/Login/google.svg"
 
