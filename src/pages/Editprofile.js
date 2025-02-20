e3ab073e2fdb062648dc34465ee811a739546095
Haeun <hahashe@handong.ac.kr>
2025-02-21 02:31:42 +0900

    feat : 프로필 가이드 모덜 버튼 위치 수정

--- src/pages/Editprofile.js
+++ src/pages/Editprofile.js
@@ -284,7 +284,10 @@ const handleClickNoShow = () => {
       <div className="newprofile-bottom-container">
         <div className="newprofile-bottom-container-size">
           <div className="newprofile-bottom-container-1">
-            <p>프로필 작성하기</p>
+            <div className='bungyung'>
+              <p>프로필 작성하기</p>
+              <img src={muluumpobtn} onClick={() => guideModalClick()} className='imgCursor'/>
+            </div>
             <span>*필수 입력 사항: 닉네임, 입학연도, 졸업연도, 전공 (한섬을 택한 경우, 직무도 필수로 입력해주세요)</span>
           </div>
 {/* 2------------------------------------------------------------ */}
@@ -400,7 +403,7 @@ const handleClickNoShow = () => {
           <div className="newprofile-bottom-container-4 box-column">
             <div className="newprofile-bottom-container-4-name">
               <span>대표 이력</span>
-              <img src={muluumpobtn} onClick={() => guideModalClick()}/>
+              
             </div>
           <div className="gridBox">
             {/* box profile값들 불러온 세팅 */}
